fa2a96c217784b82f6d3ed019bc0300955a9ad31
Sir Anthony <anthony@adsorbtion.org>
2011-01-04 21:28:24 +0500

    Modify front page. Add styles&scripts.

---
+++ anime/static/ajax.js
@@ -0,0 +1,287 @@
+//##############################################################################
+//###############################    Аякс   ####################################
+//##############################################################################
+
+var ajax = new (function(){
+    
+    var xmlHttp = null;
+    var cookie = cookies; 
+   
+    //################# Вызов аяксового обьекта.
+    this.loadXMLDoc = function(url,qry){
+        xmlHttp = null;
+        if(window.XMLHttpRequest){
+            try{
+                xmlHttp = new XMLHttpRequest();
+            }catch (e){}
+        }else if (window.ActiveXObject){
+            try{
+                xmlHttp = new ActiveXObject('Msxml2.XMLHTTP');            
+            }catch (e){
+                try{
+                    xmlHttp = new ActiveXObject('Microsoft.XMLHTTP');
+                }catch (e){}
+            }
+        }
+        
+        var cookies = cookie.get('SESSION');
+        if (qry){
+            if (xmlHttp){
+                xmlHttp.open("POST", url, true);
+                xmlHttp.onreadystatechange = handleRequestStateChange;
+                xmlHttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                /*xmlHttp.setRequestHeader("Connection", "close");                
+                if (cookie){
+                    cookies = 'SESSION=' + cookies+ ';';
+                    xmlHttp.setRequestHeader("Cookie", cookies);
+                }*/
+                xmlHttp.send(qry);
+            }    
+        }
+    }
+    
+    var setRequest = function(){
+        var mspan = document.getElementById('mspan');
+        var menu = document.getElementById('menu');
+        element.removeAllChilds(mspan)
+        element.appendChild(mspan, [element.create('p', {innerText: 'Processing Request'}),
+                                element.create('img', {src: '/templates/loader.gif'})]);
+        menu.style.display = 'block';
+    }
+    
+    //################# обработка результатов
+    var handleRequestStateChange = function(){
+        try{
+            if (!app.appajax){
+                if (xmlHttp.readyState == 1) { //Мозила
+                    setRequest();
+                }
+                if (xmlHttp.readyState == 2) { //А это специально для эстета оперы, которая статус 1 признавать не хочет.
+                    setRequest();
+                }
+            }
+            //if (xmlHttp.readyState == 3){}
+            if (xmlHttp.readyState == 4) {
+                app.appajax = null;                        
+                if (xmlHttp.status == 200){
+                    //alert(xmlHttp.responseText);
+                    //    document.getElementById('mspan').innerHTML = xmlHttp.responseText;
+                    var resp = xmlHttp.responseText.replace(/\n/gi,'');
+                    if( /^\{.*\}$/.test(resp)){
+                        resp = eval("("+resp+")");
+                        //if( /^\[.*\]$/.test(xmlHttp.responseText)){
+                        processingResult(resp);
+                        /*}else{
+                            var resp = xmlHttp.responseText;
+                            document.getElementById('mspan').innerHTML = resp;
+                        */
+                    }else if(xmlHttp.responseXML){ //Опера не отличает жсон от xml. лол.
+                        ajxedt(xmlHttp.responseXML.documentElement);                 
+                    }
+                    mfl = 1; //Сыграем на том, что это возьмет немного времени;                 
+                }else{
+                    alert(xmlHttp.status);
+                    alert("Не удалось получить данные: \u000A"+xmlHttp.statusText);
+                }
+            }
+        }catch(e){
+            alert('Caught Exception: ' + e);        
+            mfl = 1;
+            document.getElementById('menu').style.display = 'none';
+        }
+    }
+    
+    var processingResult = function(response){    
+        var mspn = document.getElementById('mspan');
+        var resp = response;
+        try{      
+            switch (resp['response']){
+            
+                case 'app':                
+                    app.retfunct(resp['text']);
+                break;
+                
+                case 'edtok':
+                /*
+                    document.getElementById('menu').style.display='none';
+                    mspn.innerHTML = 'none';
+                    edit.processResult(resp['text']);
+                */                
+                break;
+                
+                case 'editok':
+                /*
+                    mspn.innerHTML = 'База успешно обновлена';            
+                    if (!resp.text){
+                        elm.innerHTML = "";
+                    }else{                                    
+                        if(resp.text != 'notext' && elm)
+                            elm.innerHTML = resp.text;                    
+                    }
+                    timer = setTimeout("document.getElementById('menu').style.display='none';", 1500);
+                */
+                break;
+                
+                case 'error':
+                    if(elm && elm.firstChild.defaultValue){elm.innerHTML = elm.firstChild.defaultValue;}
+                    if(resp.descripion){
+                        throw new Error(resp.descripion);
+                    }else{
+                        throw new Error('Unknown error.');
+                    }
+                break;
+                
+                case 'nmform':                
+                    var inn = element.create('form',{ id: 'frm'});                
+                    var label = element.create('label',{ htmlFor: 'state', 
+                        innerText: resp.text.val });
+                    var sel = element.create('select',{id: 'stid', name: 'state',
+                        onchange: function(){sndstat(resp.text.id, resp.text.name);}
+                    });
+                    resp.text.select = (function(obj){
+                        var ret = {};
+                        for(var i = 0; i<(function(o){var n=0; for(e in o) n++; return n;})(obj); i++) ret[i] = obj[i];
+                        return ret;
+                    })(resp.text.select);
+                    element.addOption(sel, resp.text.select);                                
+                    if(resp.text.selected){                        
+                        for(var i in sel.childNodes){
+                            if( sel.childNodes[i].value == resp.text.selected)
+                                sel.childNodes[i].selected = true
+                        }
+                    }
+                    element.appendChild(inn,[label,element.create('br'),sel])
+                    if(resp.text.all){
+                        var sel = element.create('select',{id: 'stnum',
+                            onchange: function(){sndstat(resp.text.id, resp.text.name);}});
+                        var arr = new Array();
+                        for(var i=1; i<=resp.text.all; i++){arr[i] = i;}//Пиздец, а не способ!
+                        element.addOption(sel, arr);
+                        if(resp.text.cmp && sel.childNodes[resp.text.cmp]){
+                            var el = resp.text.cmp;
+                            sel.childNodes[el].selected = true;
+                        }else{sel.childNodes[0].selected = true;}
+                        sel.removeChild(sel.firstChild);                    
+                        element.appendChild(inn,[sel]);
+                    }
+                    element.removeAllChilds(mspn);
+                    element.appendChild(mspn, [inn]);
+                    sel.focus();
+                break;
+                
+                case 'frmedt':
+                    var el = document.getElementById('id'+resp.text.id).parentNode;
+                    el.style.backgroundColor = resp.text.color;                    
+                    obj = document.getElementById('numberofep' + resp.text.id);
+                    nof = obj.textContent.replace(/(\d+\/)?/, "")
+                    element.removeAllChilds(obj);
+                    if(resp.text.comm){
+                        element.appendChild(obj, [element.create('TextNode', {innerText: resp.text.comm+'/'+nof})]);
+                    }else{
+                        element.appendChild(obj, [element.create('TextNode', {innerText: nof})]);
+                    }
+                    document.getElementById('menu').style.display = 'none';
+                    element.removeAllChilds(mspn);
+                break;
+                
+                case 'egetok':
+                    if(resp.text.edt == 'yes'){
+                        var name;
+                        for(var i in resp.text){
+                            if(i == 'edt' || i == 'id'){continue;}
+                            name = i;                                         
+                        }
+                        edit.fillCont(resp.text[name], name, resp.text.id);
+                    }
+                break;
+                
+                case 'getok':                
+                    element.removeAllChilds(mspn);
+                    for(var i in resp.text.order){
+                        var curname = resp.text.order[i];
+                        if(!curname) continue;
+                        if(!resp.text[curname]) continue;
+                        var current = resp.text[curname];
+                        var capCurname = curname.substr(0,1).toUpperCase() + curname.substr(1)
+                        var label = element.create('label', { 'for': curname + resp.text.id,
+                                            innerText: capCurname + ':', name: resp.text.id});
+                        var sp = element.create('p', {'id': curname+resp.text.id, 'name': curname});
+                        var hid = ((current.tbl) ? current.tbl : "catalog");
+                        hid = element.create('input', {type: 'hidden', name: 'table', value: hid});
+                        //var edt = createElem('span',{className: 'edtl', 'name': i});
+                        /*if(!resp.text.edt && !resp.text[i][0]){continue;}
+                        if(resp.text.edt && !resp.text[i][0]){}else{}*/
+                        var num = numHash(current);
+                        for(var g=0; g<=num; g++){
+                            var cur = current[g];
+                            if(cur && isHash(cur)){
+                                var p = (curname == 'bundle') ? element.create('p',{name: cur.elemid}) :
+                                                               element.create('p',{'name': g});
+                                var s = (curname == 'bundle') ? element.create('span',{name: 'name'}) :
+                                            element.create('span',{name: 'name', innerText: encd(cur.name)});
+                                element.appendChild(sp, [p, [s]]);
+                                if(curname == 'bundle'){
+                                    var a = element.create('a',{href: '/card/'+cur.elemid+'/', innerText: encd(cur.name),
+                                                                            className: 'snone'});
+                                    if(cur.job) pclass.add(a,"s"+cur.job);
+                                    element.appendChild(s, [a])
+                                }
+                                if(cur.role){
+                                    element.appendChild(p, [
+                                            element.create('span', {name: 'role', innerText: ' as '+encd(cur.role)})]);
+                                }
+                                if(cur.comm){
+                                    element.appendChild(p, [
+                                            element.create('span', {name: 'comm', innerText: '('+encd(cur.comm)+')'})]);
+                                }
+                            }else{                                
+                                var s;
+                                if(curname == 'translation'){
+                                    s = element.create('span', {name: 'name'});
+                                    element.appendChild(s, [
+                                        element.create('span', {name: 'trstart', innerText: encd(current[0][0])})]);
+                                    if(current[0][1])
+                                        element.appendChild(s, [
+                                            element.create('span', {innerText: ' - '}),
+                                            element.create('span', {name: 'trend', innerText: encd(current[0][1])})
+                                        ]);
+                                }else{
+                                    if(curname == 'duration') current += ' min.';
+                                    s = element.create('span', {name: 'name', innerText: encd(current)});
+                                }
+                                element.appendChild(sp, [element.create('p'), [s]]);
+                            }
+                        }
+                        element.appendChild(mspn, [label, [/*edt,*/ sp, [hid]]]);
+                    }
+                break;
+                
+                case 'logfail':
+                    user.loginFail(resp.text);
+                    throw new Error("Login error");
+                break;
+                
+                case 'logok':
+                    user.loginSuccess(resp.text)
+                    window.location.reload();
+                break;
+                
+                case 'search':
+                    searcher.putResult(resp.text);
+                break;
+            }
+        }catch(e){
+            if(elm && elm.firstChild.defaultValue){elm.innerText = elm.firstChild.defaultValue;}
+            var mspan = document.getElementById('mspan');
+            element.removeAllChilds(mspan);
+            element.appendChild(mspan, [element.create('p', {innerText: e})]);
+            document.getElementById('menu').style.display = 'block';
+            if(resp.text.text != 'notext'){
+                element.appendChild(mspn, [element.create('p', {innerText: 'Server response:'}),
+                                                                      element.create('p', {innerText: resp.text})]);
+            }
+            //timer = setTimeout("document.getElementById('menu').style.display='none';", 8000);
+        }
+    }
+    
+})();
---
+++ anime/static/app.js
@@ -0,0 +1,207 @@
+//##############################################################################
+//##############################    Автодополнение   ###########################
+//##############################################################################
+
+var app = {
+        
+    appobj: null,
+    appdiv: null,
+    apparea: null,
+    keyCode: null,
+    res: new Array(),
+    appajax: null,
+    text: null,
+    selected: -1,
+    retfunct: null,
+    appurl: '/cgi-bin/app.pl',
+    timeout: null,    
+    opts: {
+        delay: 7500,
+        kdelay: 1000,
+        left: 0,
+        top: 0
+    },    
+    
+    acomp: function(obj, ar, e){        
+        
+        this.appobj = obj;
+        this.setFocus(this.appobj);
+        this.apparea = ar;
+        if(!this.appdiv){
+            this.appdiv = element.create('div', {className: 'app_div'});
+            element.appendChild(document.body, [this.appdiv]);
+        }
+        if(!e) e = window.event;
+        this.keyCode = e.keyCode;
+        switch(e.keyCode) {
+            case 38: // up
+                if(this.appdiv.style.display == 'block') this.moveSelect(-1);
+                if(e.preventDefault) e.preventDefault();
+                else e.returnValue = false;
+                break;
+            case 40: // down
+                if(this.appdiv.style.display == 'block') this.moveSelect(1);
+                if(e.preventDefault) e.preventDefault();
+                else e.returnValue = false;
+                break;
+            case 9:  // tab
+            case 13: // return
+                if(this.appdiv.style.display == 'block' && this.selected >= 0){
+                    this.selectCur();
+                    this.appobj.blur();
+                }
+                if(e.preventDefault) e.preventDefault();
+                else e.returnValue = false;
+            break;
+            default:
+                if(e.keyCode > 32) this.hideRes();
+                this.selected = -1;
+                if (this.timeout) clearTimeout(this.timeout);
+                this.timeout = setTimeout(function(){app.change();}, this.opts.kdelay);
+            break;
+        }        
+    },
+    
+    change: function(){
+        if( this.keyCode == 46 || (this.keyCode > 8 && this.keyCode < 32) ) 
+            return this.hideRes();
+        if( this.text != this.appobj.value){        
+            this.text = this.appobj.value;            
+            if(this.appobj.textLength > 2){                                
+                this.setpos(this.appdiv, this.appobj);
+                var params = {
+                    method: 'auto',
+                    value: this.appobj.value,
+                    area: this.apparea
+                }
+                this.getarr(this.autores, params);                 
+            }else{
+                this.hideRes();
+            }
+        }        
+    },
+    
+    moveSelect: function(step){
+        
+        if (this.timeout) clearTimeout(this.timeout);
+        var lis = getChilds(this.appdiv.firstChild);
+        element.downTree(function(el){pclass.remove(el, "app_over");}, this.appdiv.firstChild);                         
+        this.selected += step;
+        if(this.selected <= 0){
+            this.selected = 0;
+        }else if(this.selected >= lis.length){
+            this.selected = lis.length - 1;
+        }
+        pclass.add(lis[this.selected], "app_over");
+    },
+
+    getarr: function(funct,params){
+        
+        this.retfunct = funct;
+        this.appajax = '1';
+        qw = 'method='+
+        ((params['method']) ? params['method'] : "none") +
+        ((params['value']) ? "&string="+params['value'] : "") +
+        ((params['area']) ? "&area="+params['area']: "") +
+        ((params['local']) ? "&local="+params['local']: "");                        
+        loadXMLDoc(this.appurl, qw);
+    },
+    
+    autores: function(text){
+        this.res = eval(text);
+        if(this.appdiv.firstChild)
+            this.appdiv.removeChild(this.appdiv.firstChild);
+        var ul = document.element.createent('ul');                
+        for (var i=0; i < this.res.length; i++) {
+            var li = element.create('li', {
+                innerHTML: this.highLight(this.res[i].name),  // change to append
+                selectValue: this.res[i],
+                onmouseover: function(){
+                    if(app.timeout) clearTimeout(app.timeout);
+                    var sel = (function(obj,c){
+                        for(var i=0; i<c.length; i++){if(c[i]==obj)    return i;}
+                    })(this, this.parentNode.childNodes);
+                    if(app.selected >= 0){
+                        var old = this.parentNode.childNodes[app.selected];
+                        pclass.remove(old, "app_over");
+                    }
+                    pclass.add(this, "app_over");
+                    app.selected = sel;
+                },
+                onmouseout: function(){
+                    app.timeout = setTimeout(function(){app.hideRes();}, app.opts.delay);
+                    pclass.remove(this, "app_over");
+                    app.selected = -1;
+                },
+                onclick: function(e){
+                    if(e.stopPropagation) e.stopPropagation();
+                    else e.cancelBubble = true;
+                    if(e.preventDefault) e.preventDefault();
+                    else e.returnValue = false;                     
+                    app.selectCur(this);
+                }
+            });
+            element.appendChild(ul,[li]);
+        }                
+        element.appendChild(this.appdiv, [ul]);
+        this.retfunct = null;        
+        this.appdiv.style.display = 'block';
+        if (this.timeout) clearTimeout(this.timeout);
+        this.timeout = setTimeout(function(){app.hideRes();}, this.opts.delay);  
+    },
+    
+    hideRes: function(){
+        if (this.timeout) clearTimeout(this.timeout);
+        if(this.appdiv.firstChild)
+            this.appdiv.removeChild(this.appdiv.firstChild);
+        this.appdiv.style.display = 'none';            
+    },
+    
+    hideResult: function() {
+        if (this.timeout) clearTimeout(this.timeout);
+        this.timeout = setTimeout(function(){app.hideRes();}, 150);
+    },
+    
+    selectCur: function(item){        
+        if (this.timeout) clearTimeout(this.timeout);
+        var s;
+        if(item){
+            s = item;            
+        }else{
+            var f = function(obj){if(pclass.hasClass(obj, "app_over")) return obj;}
+            s = element.downTree(f, this.appdiv.firstChild, 1);
+        }
+        this.appobj.value = ((s.selectValue) ? s.selectValue.name : s.innerHTML);
+        if(s.selectValue && s.selectValue.id){
+            var id = ((this.appobj.nextSibling.name == "pid") ? this.appobj.nextSibling : (function(obj){
+                    var el = element.create('input', {type: 'hidden', name: 'pid'});
+                    obj.parentNode.insertBefore(el, obj.nextSibling);
+                    return el;})(this.appobj));
+            id.value = s.selectValue.id;
+        }
+        this.text = this.appobj.value;        
+        this.hideRes();
+    },    
+    
+    highLight: function(str){
+        if(this.text){
+            var re = new RegExp("("+this.text+")", "ig");            
+            return str.replace(re, "<b>$1</b>");
+        }
+        return str;
+    },
+    
+    setFocus: function(obj){
+        if(!this.appobj.onfocus)
+            this.appobj.onfocus = function(){this.hasFocus = true;};
+        if(!this.appobj.onblur)
+            this.appobj.onblur = function(){this.hasFocus = false; app.hideResult();};
+    },
+        
+    setpos: function(){
+        var os = getOffset(this.appobj);
+        this.appdiv.style.top = os.top+this.appobj.offsetHeight+this.opts.top+'px';
+        this.appdiv.style.left= os.left-this.appobj.offsetLeft+this.opts.left+'px';
+    }
+    
+}
