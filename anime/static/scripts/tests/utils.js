fbf897dca0187621f9e90239f8cb108abdb90d7b
SirAnthony <anthony@adsorbtion.org>
2014-02-24 13:19:50 +0600

    Forgot a file.

---
+++ anime/static/scripts/tests/utils.js
@@ -0,0 +1,41 @@
+/*
+ * This file is part of Anicat.
+ *
+ * Anicat is distributed under the terms of Anicat License.
+ * See <http://www.anicat.net/LICENSE/> for feature details.
+ *
+ * Javascript tests utils
+ *
+ */
+
+define(['base/request_processor'], function (RequestProcessor){
+
+	function set_processor(done, func){
+		RequestProcessor.prototype._parsed = function() {
+			RequestProcessor.prototype._parsed = function() {}
+			func && func()
+			done()
+		}
+	}
+
+	function processor_setter(func) {
+		func = func || function() {}
+		return function() {
+			RequestProcessor.prototype._parsed = func
+		}
+	}
+
+	function mocha_it(call, after, condition) {
+		condition = condition || 'should pass'
+		it(condition, function(done){
+			set_processor(done, after)
+			call()
+		})
+	}
+
+	return {
+		it: mocha_it,
+		set_processor: set_processor,
+		processor_setter: processor_setter
+	}
+})
